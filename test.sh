14a3b186de2de0273c3176ce4bb97311ad106d22
Emilio Forrer <dev.emilio.forrer@gmail.com>
2020-05-17 04:34:30 -0600

    ✅ Add simple command test

---
+++ test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e;
+
+# TODO: Improve the way to test the commands 
+
+echo "Git Version"
+docker run --rm emilioforrer/ci-tools:latest git --version
+echo "------------------------"
+echo ""
+echo "Bash Version"
+docker run --rm emilioforrer/ci-tools:latest bash --version
+echo "------------------------"
+echo ""
+echo "yq Version"
+docker run --rm emilioforrer/ci-tools:latest yq --version
+echo "------------------------"
+echo ""
+echo "jq Version"
+docker run --rm emilioforrer/ci-tools:latest jq --version
+echo "------------------------"
+echo ""
+echo "curl Version"
+docker run --rm emilioforrer/ci-tools:latest curl --version
+echo "------------------------"
+echo ""
+echo "docker Version"
+docker run --rm emilioforrer/ci-tools:latest docker --version
+echo "------------------------"
+echo ""
+echo "docker-compose Version"
+docker run --rm emilioforrer/ci-tools:latest docker-compose --version
+echo "------------------------"
+echo ""
+echo "kind Version"
+docker run --rm emilioforrer/ci-tools:latest kind --version
+echo "------------------------"
+echo ""
+echo "kind Version"
+docker run --rm emilioforrer/ci-tools:latest helm version
+echo "------------------------"
+echo ""
+echo "kubectl Version"
+docker run --rm -v "$KUBECONFIG:$KUBECONFIG" \
+           -e KUBECONFIG=$KUBECONFIG \
+           emilioforrer/ci-tools:latest kubectl version
+echo "------------------------"
+echo ""
+echo "vault Version"
+docker run --rm emilioforrer/ci-tools:latest vault --version
+echo "------------------------"
+echo ""
+echo "1passwsord Version"
+docker run --rm emilioforrer/ci-tools:latest op --version
+echo "------------------------"
+echo ""
+echo "semver Status"
+docker run --rm emilioforrer/ci-tools:latest semver get major 1.0.0 1>/dev/null && echo "OK"
+echo "------------------------"
+echo ""
+echo "argocd Status"
+docker run --rm emilioforrer/ci-tools:latest argocd 1>/dev/null && echo "OK"
+echo "------------------------"
+echo ""
